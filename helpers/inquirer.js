618aa9637d5cca5b359d6815d428e6c4396dcb99
Flavio  Fabian Espeche Nieva <m25511218@subinfo.local>
2021-06-22 12:22:23 -0300

    Commit inicial

---
+++ helpers/inquirer.js
@@ -0,0 +1,15 @@
+const inquirer = require('inquirer');
+require('colors');
+
+const inquirerMenu = async () => {
+    console.clear();
+    console.log('*******************************'.blue);
+    console.log('     Seleccione una opción'.blink);
+    console.log('*******************************'.blue);
+
+    
+}
+
+module.exports = {
+    inquirerMenu,
+}
