2baa70a2d1181bb25618a7566790cf1b850f615d
Flavio  Fabian Espeche Nieva <m25511218@subinfo.local>
2021-06-22 14:29:22 -0300

    Hasta definición de modelos

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 require("colors");
-console.clear();
+const { inquirerMenu, pausa } = require("./helpers/inquirer");
+const Tareas = require("./models/tareas");
 
 const main = async () => {
-  
+  let opt = "";
+  const tareas = new Tareas();
+  do {
+    opt = await inquirerMenu();
+    await pausa();
+  } while (opt !== "0");
 };
 
 main();
--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -1,15 +1,66 @@
-const inquirer = require('inquirer');
-require('colors');
+const inquirer = require("inquirer");
+require("colors");
+
+const preguntas = [
+  {
+    type: "list",
+    name: "opcion",
+    message: "¿Qué desea hacer?",
+    choices: [
+      {
+        value: "1",
+        name: `1. Crear Tarea`,
+      },
+      {
+        value: "2",
+        name: `2. Listar Tareas`,
+      },
+      {
+        value: "3",
+        name: `3. Listar Tareas completadas`,
+      },
+      {
+        value: "4",
+        name: `4. Listar Tareas pendientes`,
+      },
+      {
+        value: "5",
+        name: `5. Completar Tareas`,
+      },
+      {
+        value: "6",
+        name: `6. Borrar tarea`,
+      },
+      {
+        value: "0",
+        name: `0. Salir`,
+      },
+    ],
+  },
+];
 
 const inquirerMenu = async () => {
-    console.clear();
-    console.log('*******************************'.blue);
-    console.log('     Seleccione una opción'.blink);
-    console.log('*******************************'.blue);
+  console.clear();
+  console.log("*******************************".cyan);
+  console.log("     Seleccione una opción".cyan);
+  console.log("*******************************".cyan);
 
-    
-}
+  const { opcion } = await inquirer.prompt(preguntas);
+
+  return opcion;
+};
+
+const pausa = async () => {
+  await inquirer.prompt([
+    {
+      type: "input",
+      name: "ok",
+      message: `Presione ${"ENTER".blue} para continuar`,
+    },
+  ]);
+};
 
 module.exports = {
-    inquirerMenu,
-}
+  inquirerMenu,
+  pausa,
+};
